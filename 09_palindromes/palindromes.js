72a9bbaf40375b72063f74c070dbf3b1a2b721bc
Rabi Alaya <rabialaya@gmail.com>
2022-05-31 12:20:59 -0500

    solved 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let reverse_str = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        if ((/[a-zA-Z]/).test(str.charAt(i))) {
+            reverse_str += str.charAt(i);
+        }
+    }
+    clean_str = ''
+    for (let i = 0; i < str.length; i++) {
+        if ((/[a-zA-Z]/).test(str.charAt(i))) {
+            clean_str += str.charAt(i);
+        }
+    }
+    return reverse_str.toLowerCase() === clean_str.toLowerCase();
 };
 
 // Do not edit below this line
