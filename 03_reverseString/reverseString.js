c58786fd6b53230793a9655a2c4982c9f49203d2
Rabi Alaya <rabialaya@gmail.com>
2022-05-30 22:05:16 -0500

    solved 1-3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let result = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        result += str.charAt(i);
+    }
+    console.log(result);
+    return result;
 };
 
 // Do not edit below this line
