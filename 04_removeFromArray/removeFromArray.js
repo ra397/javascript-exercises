a0d94552ca4ee276e3be1d0c4c92d53bbc0f38aa
Rabi Alaya <rabialaya@gmail.com>
2022-05-31 11:42:07 -0500

    Solved 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...nums) {
+    parameters = []
+    for (let num of nums) {
+       parameters.push(num);
+    }
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < parameters.length; j++) {
+        if (array[i] === parameters[j]) {
+          array.splice(i, 1);
+          i --;
+        }
+      }
+    }
+    return array;
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;
