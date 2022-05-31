8bd157caed79eae3299e5ae56771b8cd07d521a8
Rabi Alaya <rabialaya@gmail.com>
2022-05-31 12:57:13 -0500

    Solved 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let maxPerson = NaN;
+    let maxAge = -1;
+    for (let person of people) {
+        let age;
+        // calculate person's age
+        if (person.yearOfDeath != undefined) {
+            age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            age = (new Date().getFullYear()) - person.yearOfBirth;
+        }
+        if (age > maxAge) {
+            maxAge = age;
+            maxPerson = person;
+        }
+    }
+    return maxPerson;
 };
 
 // Do not edit below this line
