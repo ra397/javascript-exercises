f06925d5f81ce7cd06a055e08947a2d1c4bb3d9e
Rabi Alaya <rabialaya@gmail.com>
2022-05-31 12:41:49 -0500

    Solved 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    if (member < 0) {
+        return "OOPS"
+    }
+    let array = [1, 1];
+    if (member === 1 || member === 2) {
+        return 1;
+    }
+    for (let i = 0; i < member - 1; i++) {
+        array.push(array[i] + array[i + 1])
+    }
+    array.pop();
+    return array.pop();
 };
 
 // Do not edit below this line
