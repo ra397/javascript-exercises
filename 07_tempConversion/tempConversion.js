fe32dc34384d7655e427a465514015362e8d5856
Rabi Alaya <rabialaya@gmail.com>
2022-05-31 12:00:50 -0500

    Solved 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(f_temp) {
+  return Math.round(((f_temp - 32) * (5/9)) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(c_temp) {
+  return Math.round((c_temp * (9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
